060f2745af4101b59b8fc6db89bb3577a48f8435
Bak0n <Bak0n@users.noreply.github.com>
2017-09-30 15:50:45 -0400

    Hello world for Bork.js
    Outputs "heck" to the console.

---
+++ examples/heckworld.js
@@ -0,0 +1,11 @@
+// Requires bork.js
+// Outputs "heck"
+
+bork_new(0); bork_new(0); bork_new(1); bork_new(1); bork_new(1);
+bork_set( ); bork_reset();
+bork_new(0); bork_new(0); bork_new(1); bork_new(0); bork_new(0);
+bork_set( ); bork_reset();
+bork_new(0); bork_new(0); bork_new(0); bork_new(1); bork_new(0); 
+bork_set( ); bork_reset();
+bork_new(0); bork_new(1); bork_new(0); bork_new(1); bork_new(0);
+bork_set( ); bork_reset(); bork_print();
