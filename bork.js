ecc8c9db81b1881338deb81ac2db91be74243b84
Bak0n <Bak0n@users.noreply.github.com>
2017-09-30 20:59:40 -0400

    bork v0.2.0
    new `bork_change()` function to change text of id w/ DOM

--- bork.js
+++ bork.js
@@ -1,4 +1,5 @@
 /* BORK.JS */
+
 // Set bork variables
 var bork_table = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", ".", "!", "?", ",", "\n"]
 var bork_borks = [];
